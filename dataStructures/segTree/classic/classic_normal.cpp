ccf8d9843b714446f09f810056a33355930ad9da
VictorLamarca <victordslamarca@gmail.com>
2020-04-14 19:22:43 -0300

    Unificar segs - soma com maximo comentado

--- dataStructures/segTree/classic/classic_normal.cpp
+++ dataStructures/segTree/classic/classic_normal.cpp
@@ -6,21 +6,19 @@ using namespace std;
 typedef long long ll;
 
 /*
+	Seg de minimo
+	Para ser de soma, basta descomentar linhas comentadas
+	
     Lembrar de 4*N no tamanho da seg
-    
-    Lembrar de dar build
     
     Nós da seg [1,4*N-1], e nós do vetor [0,n-1]
     
     intervalos abertos [l,r)
 */
 
-const int N = 1e5+10;
-
-ll s[4*N];
-ll v[N];
-int n, q;
-
+template<int n> struct Seg{
+	ll *v, *s;
+	
 void build(int no = 1, int l = 0, int r = n){
     if(r-l==1){ 
         s[no] = v[l];
@@ -30,36 +28,52 @@ void build(int no = 1, int l = 0, int r = n){
     build(2*no,l,mid);
     build(2*no+1,mid,r);
     s[no] = min(s[2*no],s[2*no+1]);
+    //s[no] = s[2*no]+s[2*no+1];
 }
 
-//mudar o valor, v[i] = x (nao aumentar em x)
-void modify(int i, ll x, int no = 1, int l = 0, int r = n){
+Seg(ll *vb, ll *sb){
+	v = vb;
+	s = sb;
+	build();
+}
+
+//Muda o valor, v[i] = x
+void update(int i, ll x, int no = 1, int l = 0, int r = n){
     if(r-l==1){
-        s[no] = v[l] = x;
+        s[no] = x; // se quiser fazer upd de incremento tal que v[i] += x, basta mudar = por += aqui
         return;
     }
     int mid = (l+r)/2;
-    if(i<mid) modify(i,x,2*no,l,mid);
-    else modify(i,x,2*no+1,mid,r);  
-    s[no] = min(s[2*no],s[2*no+1]);
+    if(i<mid) update(i,x,2*no,l,mid);
+    else update(i,x,2*no+1,mid,r);  
+    s[no] = min(s[2*no],s[2*no+1]);  
+    //s[no] = s[2*no]+s[2*no+1];
 }
 
 ll query(int lq, int rq, int no = 1, int l = 0, int r = n){
-    if(rq<=l or r<=lq) return INT_MAX; //totalmente fora
+    //if(rq<=l or r<=lq) return 0; //totalmente fora
+    if(rq<=l or r<=lq) return LLONG_MAX;
     if(lq<=l and r<=rq) return s[no]; //range q estou totalmente dentro do range da query
     int mid = (l+r)/2;
     return min(query(lq,rq,2*no,l,mid),query(lq,rq,2*no+1,mid,r));
+    //return query(lq,rq,2*no,l,mid) + query(lq,rq,2*no+1,mid,r);
 }
+	
+};
+
+const int N = 1e5+10;
+
+ll s[4*N];
+ll v[N];
+int n, q;
 
 //resolve problema em: https://www.hackerearth.com/pt-br/practice/data-structures/advanced-data-structures/segment-trees/tutorial/
 
 int main(){
-
     cin >> n >> q;
     
     fr(i,n) scanf("%lld", v+i);
-    
-    build();
+    Seg<N> seg(v,s);
     
     fr(qq,q){
         char c;
@@ -68,15 +82,14 @@ int main(){
             int l, r;
             scanf("%d%d", &l, &r);
             l--;
-            printf("%lld\n", query(l,r));
+            printf("%lld\n", seg.query(l,r));
         } else{
             int p, x;
             scanf("%d%d", &p, &x);
             p--;
-            modify(p,x);
+            seg.update(p,x);
         }
     }
-	
 	return 0;
 }
 
--- dataStructures/segTree/classic/lazy.cpp
+++ dataStructures/segTree/classic/lazy.cpp
@@ -6,31 +6,25 @@ typedef long long ll;
 #define fr(i,n) for(int i = 0; i<n; i++)
 
 /* 
+	Seg lazy de soma
+	para ser de minimo descomente linhas comentadas
+	
     Lembrar de 4*N no tamanho da seg e lazy
-    
-    Lembrar de dar build
     
     Nós da seg [1,4*N-1], e nós do vetor [0,n-1]
     
-    intervalos abertos [l,r)
-    
-    condição base/terminal na query eh suficiente pois quando estou em folha (elemento único) com ctz cai na condição base (ou está completamente dentro ou completamente fora, pois é só um elemento!)
-    
-    v[id] nao estara atualizado, para recuperalo preciso passar o s[no] com no referente ao id
+    intervalos abertos [l,r) para fazer query e update!
 */
 
-const int N = 1e5+10;
+//se em um vector<Seg> precisar de tamanho variavel por seg
+//da pra fazer if(r==-1) r = tam[id] onde id eh um tam global
+//ou fazer = um nglobal e mudar o n global td vez q for usar
+//alem disso, usar vector, pois eh variavel
+template<int N> struct Seg{
 
-ll s[4*N];
+ll *v, *s, *lazy;
 
-//lazy nao precisa ter valor da soma total a ser passada, apenas o x que será multiplicado
-ll lazy[4*N];
-
-ll v[N];
-
-int n, q;
-
-void build(int no = 1, int l = 0, int r = n){
+void build(int no = 1, int l = 0, int r = N){
     if(r-l==1){
         s[no] = v[l];
         return;
@@ -39,11 +33,24 @@ void build(int no = 1, int l = 0, int r = n){
     build(2*no,l,mid);
     build(2*no+1,mid,r);
     s[no] = s[2*no]+s[2*no+1];
+    //s[no] = max(s[2*no],s[2*no+1]);
+}
+
+Seg(ll *vb, ll *sb, ll *lazyb){
+	v = vb, s = sb, lazy = lazyb;
+	//apenas o v precisa estar correto q o s sera buildado
+	//ja o lazy preciso lembrar de resetar
+	fr(i,4*N){
+        lazy[i] = 0;
+    }
+	build();
 }
 
 //seto lazy no nó (marco que filhos não foram propagados)
+//mas s[no] esta correto
 void updlazy(int no, int l, int r, ll x){
     s[no] += (r-l)*x;
+    //s[no] += x;
     lazy[no] += x;
 }
 
@@ -55,7 +62,7 @@ void pass(int no, int l, int r){
     lazy[no] = 0;
 }
 
-void upd(ll x, int lup, int rup, int no = 1, int l = 0, int r = n){
+void upd(ll x, int lup, int rup, int no = 1, int l = 0, int r = N){
     if(rup<=l or r<=lup) return;
     if(lup<=l and r<=rup){
         updlazy(no,l,r,x);
@@ -66,33 +73,41 @@ void upd(ll x, int lup, int rup, int no = 1, int l = 0, int r = n){
     upd(x,lup,rup,2*no,l,mid);
     upd(x,lup,rup,2*no+1,mid,r);
     s[no] = s[2*no] + s[2*no+1];
+    //s[no] = max(s[2*no],s[2*no+1]);
 }
 
-ll query(int lq, int rq, int no = 1, int l = 0, int r = n){
-    if(rq<=l or r<=lq) return 0;
+ll query(int lq, int rq, int no = 1, int l = 0, int r = N){
+    if(rq<=l or r<=lq) return 0; //mudar return se for de minimo
     if(lq<=l and r<=rq){
         return s[no];
     } 
     pass(no,l,r);
     int mid = (l+r)/2;
     return query(lq,rq,2*no,l,mid) + query(lq,rq,2*no+1,mid,r);
+    //return max(query(lq,rq,2*no,l,mid),query(lq,rq,2*no+1,mid,r));
 }
+};
 
-//solves problem: https://www.spoj.com/problems/HORRIBLE/
+const int N = 1e5+10;
+
+//v[id] nao estara atualizado, para recuperalo preciso passar o s[no] com no referente ao id
+ll v[N];
+ll s[4*N];
+//lazy nao precisa ter valor da soma total a ser passada, apenas o x que será multiplicado
+ll lazy[4*N];
+
+//Seg soma solves: https://www.spoj.com/problems/HORRIBLE/
+//Seg max solves: https://codeforces.com/gym/102307/problem/E
 
 int main(){
     int t;
     cin >> t;
     
+    int n, q;
     fr(tt,t){
         scanf("%d%d", &n, &q);
-        
         //nesse problema v começa com td zero, portanto nem preciso mexer nele pois nao muda entre casos msm
-        
-        fr(i,4*n){
-            lazy[i] = s[i] = 0;
-        }
-        
+        Seg<N> seg(v,s,lazy);
         fr(qq,q){
             int c;
             scanf("%d", &c);
@@ -100,16 +115,15 @@ int main(){
                 int l, r, x;
                 scanf("%d%d%d", &l, &r, &x);
                 l--;
-                upd(x,l,r);
+                seg.upd(x,l,r);
             } else{
                 int l, r;
                 scanf("%d%d", &l, &r);
                 l--;
-                printf("%lld\n", query(l,r));
+                printf("%lld\n", seg.query(l,r));
             }
         }
     }
-	
 	return 0;
 }
 
