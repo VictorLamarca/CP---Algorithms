c717bc1388d39fd58028b846c500a1328d53d54e
VictorLamarca <victordslamarca@gmail.com>
2020-09-08 12:52:13 -0300

    Setup for competitive programming

---
+++ tecnico/setup_maratona/temp.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fr(i,n) for(int i = 0; i<n; i++)
+#define sz(v) (int)(v.size())
+#define prin(a) cout << #a << " = " << a << endl
+#define prinv(v) cout << #v << " = "; for(auto it : v) cout << it << ", "; cout << endl
+#define all(v) (v).begin(),(v).end()
+
+typedef long long ll;
+
+#define rmin(a,b) a = min<ll>(a,b)
+#define rmax(a,b) a = max<ll>(a,b)
+
+#define fi first
+#define se second
+
+int main(){
+	//ios::sync_with_stdio(0); cin.tie(0);
+}
