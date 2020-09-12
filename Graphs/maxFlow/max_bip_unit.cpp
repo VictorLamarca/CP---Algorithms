eaa71bbc92390e7230840aaff164f6a60f196ab6
VictorLamarca <victordslamarca@gmail.com>
2020-09-12 18:33:55 -0300

    Comment on bipartite matching

--- Graphs/maxFlow/max_bip_unit.cpp
+++ Graphs/maxFlow/max_bip_unit.cpp
@@ -1,8 +1,44 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+#define fr(i,n) for(int i = 0; i<n; i++)
+typedef long long ll;
 
-#define ll long long
+//complexidade O(E*V) -> supoe fluxo unitario!!!
+
+//M é o numero de candidatos e N o numero de vagas - por isso bipartido!
+int n, m; // em problemas de grafo quadrado lembrar de fazer m = n
+#define N 3030
+#define M 11100
+
+//g[i] = j (indice no : i (0 a M-1) "source" e it : j "dest" (0 a N-1)
+vector <int> g[M]; //pra onde aplicam cada um dos M cand
+bool vis[N];
+int match[N]; // vaga i (das N) vai para candidato match[i] (de 0 a M-1) (ou -1 se ngm vai para vaga)
+
+bool match_vertex(int u) {
+    for (int v : g[u])
+        if (!vis[v]) {
+            vis[v] = 1;
+            if (match[v] < 0 or match_vertex(match[v])) {
+                match[v] = u;
+                return 1;
+            }
+        }
+    return 0;
+}
+
+int max_match() {
+    fr(i,n) match[i] = -1;
+    int result = 0;
+    fr(u,m){
+        fr(i,n) vis[i] = 0;
+        if (match_vertex(u)) result++;
+    }
+    return result;
+}
+
+//--------------------------------------------------------------------------
 
 //DESCONSIDERAR
 //variveis especificas desse problema
@@ -10,45 +46,7 @@ ll x[3030], y[3030];
 set < ll > nums;
 map < ll, int > comp;
 
-//prob original:
-//http://codeforces.com/gym/101485/standings/friends/true
-
-//--------------------------------------------------------------------------
-
-//complexidade O(E*V) -> supoe fluxo unitario!!!
-
-//M é o numero de candidatos e N o numero de vagas
-int n, m; // em problemas de grafo quadrado lembrar de fazer m = n
-#define N 3030
-#define M 11100
-
-//g[i] = j (indice no : i (0 a M-1) "source" e it : j "dest" (0 a N-1)
-vector < int > g[M]; //pra onde aplicam cada um dos M cand
-bool vis[N];
-int match[N]; // vaga i (das N) vai para candidato match[i] (de 0 a M-1)
-
-
-bool match_vertex(int u) {
-    for (int v : g[u])
-        if (!vis[v]) {
-            vis[v] = true;
-            if (match[v] < 0 || match_vertex(match[v])) {
-                match[v] = u;
-                return true;
-            }
-        }
-    return false;
-}
-
-int max_match() {
-    memset(match, -1, sizeof match);
-    int result = 0;
-    for (int u = 0; u < m; u++) {
-        memset(vis, 0, sizeof vis);
-        if (match_vertex(u)) result++;
-    }
-    return result;
-}
+//solves problem E from: http://codeforces.com/gym/101485/standings/friends/true
 
 //--------------------------------------------------------------------------
 
