ff34979c9957308a1dd0dc5f234708324f7f8869
VictorLamarca <victordslamarca@gmail.com>
2021-03-23 20:28:35 -0300

    refactor kmp

--- strings/KMP/preffix_function.cpp
+++ strings/KMP/preffix_function.cpp
@@ -2,21 +2,20 @@
 using namespace std;
 
 #define fr(i,n) for(int i = 0; i<n; i++)
+#define sz(v) (int)(v.size())
 
 //solves https://onlinejudge.org/index.php?option=onlinejudge&page=show_problem&problem=2447
 
 vector<int> fpref(string &s){
 	vector<int> pref(s.size());
 	
-	for(int i = 1; i<s.size(); i++){
+	for(int i = 1; i<sz(s); i++){
 		int t = pref[i-1];
 		while(t and s[i]!=s[t]) t = pref[t-1];
 		if(s[i]==s[t]) t++;
 		pref[i] = t;
 	}
-	
 	return pref;
-
 }
 
 int main(){
