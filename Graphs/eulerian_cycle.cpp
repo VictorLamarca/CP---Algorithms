5a61106adf941e3bf5efc8cd412183731ffd08b3
VictorLamarca <victordslamarca@gmail.com>
2021-04-15 17:14:01 -0300

    Removing unnecessary macros from eulerian_path.cpp

--- Graphs/eulerian_cycle.cpp
+++ Graphs/eulerian_cycle.cpp
@@ -3,17 +3,6 @@ using namespace std;
 
 #define fr(i,n) for(int i = 0; i<n; i++)
 #define sz(v) (int)(v.size())
-#define prin(a) cout << #a << " = " << a << endl
-#define prinv(v) cout << #v << " = "; for(auto it : v) cout << it << ", "; cout << endl
-#define all(v) (v).begin(),(v).end()
-
-typedef long long ll;
-
-#define rmin(a,b) a = min<ll>(a,b)
-#define rmax(a,b) a = max<ll>(a,b)
-
-#define fi first
-#define se second
 
 //solves https://cses.fi/problemset/task/1691/
 
