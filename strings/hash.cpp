633c04e9863282ad5492ce20da1b280d733537b0
VictorLamarca <victordslamarca@gmail.com>
2021-06-05 23:00:46 -0300

    hash ull

--- strings/hash.cpp
+++ strings/hash.cpp
@@ -3,26 +3,17 @@ using namespace std;
 
 #define fr(i,n) for(int i = 0; i<n; i++)
 #define sz(v) (int)(v.size())
-#define prin(a) cout << #a << " = " << a << endl
-#define prinv(v) cout << #v << " = "; for(auto it : v) cout << it << ", "; cout << endl
 #define all(v) (v).begin(),(v).end()
-
 typedef long long ll;
+
+//solves https://codeforces.com/contest/1535/submission/118546615
+
+const int N = 2e5+10;
+
 typedef unsigned long long ull;
 
-#define rmin(a,b) a = min<ll>(a,b)
-#define rmax(a,b) a = max<ll>(a,b)
-
-#define fi first
-#define se second
-
-//solves https://codeforces.com/contest/1466/problem/G
-
-const int N = 2e6+10;
-
-template <class T>
-T fp(T x, long long e) {
-	T ans(1);
+ull fpull(ull x, ull e) {
+	ull ans = 1;
 	for(; e > 0; e /= 2) {
 		if(e & 1) ans = ans * x;
 		x = x * x;
@@ -30,49 +21,43 @@ T fp(T x, long long e) {
 	return ans;
 }
 
-template <class T>
-T fp2(T base, T exp){
-	if(exp==0) return T(1);
-	T mid = fp(base,exp/2);
-	if(exp%2) return mid*mid*base;
-	return mid*mid;
-}
+mt19937 rng(time(0));
 
-const ll mod = round(1e9)+7;
-struct mb {
-	mb(int v = 0) : val(v < 0 ? v + mod : v) {}
-	mb(ll v){ val = (v%mod+mod)%mod; }
-	int val;
- 
-	void operator += (mb o) { *this = *this + o; }
-	void operator -= (mb o) { *this = *this - o; }
-	void operator *= (mb o) { *this = *this * o; }
-	mb operator * (mb o) { return (int)((long long) val * o.val % mod); }
-	//mb operator / (mb o) { return *this * fp(o, mod - 2); }
-	//bool operator == (mb o) { return val==o.val; } //usar soh para hashes
-	mb operator + (mb o) { return val + o.val >= mod ? val + o.val - mod : val + o.val; }
-	mb operator - (mb o) { return val - o.val < 0 ? val - o.val + mod : val - o.val; }
-};
-
-ll n, q;
-string s0, t;
-
-vector<ull> perm;
+vector<int> perm;
 ull p27[N];
 ull inv27[N];
 
+void init_hash(int n){
+	fr(i,26) perm.push_back(i+1);
+	shuffle(all(perm),rng);
+	p27[0] = inv27[0] = 1;
+	for(int i = 1; i<n; i++){
+		p27[i] = 27*p27[i-1];
+		inv27[i] = fpull(p27[i],-1);
+	}
+}
+
+/*
+	Calcula hash de intervalos da string 
+		primeira letra é digito menos significativo
+		string de lowercase english letters
+		Base 27 é usada, cada letra é mapeada para [1,26], nao tem 0
+		Modulo é (1<<64) - unsigned long long
+*/
 struct meuhash{
 	vector<ull> pref;
+	meuhash(){}
 	meuhash(string &s){
 		assert(sz(s)<N);
 		assert(p27[1]*inv27[1]==1);
+		pref.resize(sz(s));
 		ull cur = 0;
 		fr(i,sz(s)){
-			cur = cur + p27[i]*perm[s[i]-'a'];
-			pref.push_back(cur);
+			cur += p27[i]*perm[s[i]-'a'];
+			pref[i] = cur;
 		}
 	}
-	
+	//intervalo fechado [l,r]
 	ull gethash(int l, int r){
 		assert(l<=r and l>=0 and l<sz(pref) and r>=0 and r<sz(pref));
 		l--;
@@ -83,98 +68,76 @@ struct meuhash{
 		}
 		return ans;
 	}
-};
+}; //end hash
 
-vector<int> matid[30];
-vector<mb> matpref[30];
+vector<ull> mat[610][610];
+
+int fcnt(ull cur, int l, int r){
+	return upper_bound(all(mat[l][r]),cur)-lower_bound(all(mat[l][r]),cur);
+}
 
 int main(){
 	ios::sync_with_stdio(0); cin.tie(0);
-	cin >> n >> q; 
-	cin >> s0 >> t;
 	
-	srand(time(0));
+	init_hash(N);
 	
-	//init hash
-	fr(i,26) perm.push_back(i+1);
-	random_shuffle(all(perm));
-	p27[0] = inv27[0] = 1;
-	for(int i = 1; i<N; i++){
-		p27[i] = 27*p27[i-1];
-		inv27[i] = fp2(p27[i],((ull)0)-1);
+	ll n; cin >> n;
+	map<string,vector<string>> mp;
+	fr(i,n){
+		string s; cin >> s;
+		string aux = s;
+		sort(all(aux));
+		mp[aux].push_back(s);
 	}
 	
+	ll ans = n*(n-1)/2*1337;
 	
-	//calcular mats
-	fr(i,sz(t)){
-		int ic = t[i]-'a';
-		mb cnt;
-		if(!matpref[ic].empty()){
-			cnt = matpref[ic].back();
-			ll dif = i+1-matid[ic].back();
-			cnt *= fp(mb(2),dif);
-			cnt += 1;
+	for(auto &[s_sort,v] : mp){
+		ans -= (ll)sz(v)*(sz(v)-1)/2*1335;
+		int tam = sz(v[0]);
+		if(sz(v)<=600){
+			fr(i,sz(v)) fr(j,i){
+				if(i==j) continue;
+				int l = 0, r = tam-1;
+				while(l<tam and v[i][l]==v[j][l]) l++;
+				while(r>=0 and v[i][r]==v[j][r]) r--;
+				int ok = 0;
+				for(int k = l+1; k<=r; k++){
+					if(v[i][k]<v[i][k-1]) break;
+					if(k==r) ok = 1;
+				}
+				if(!ok) for(int k = l+1; k<=r; k++){
+					if(v[j][k]<v[j][k-1]) break;
+					if(k==r) ok = 1;
+				}
+				if(ok) ans--;
+			}
 		} else{
-			cnt = 1;
+			vector<meuhash> hashes(sz(v));
+			fr(i,sz(v)) hashes[i] = meuhash(v[i]);
+			fr(r,tam) fr(l,r+1) mat[l][r].clear();
+			fr(i,sz(v)){
+				fr(r,tam) fr(l,r+1){
+					ull cur = 0;
+					if(l) cur = hashes[i].gethash(0,l-1);
+					if(r<tam-1) cur += p27[l]*hashes[i].gethash(r+1,tam-1);
+					mat[l][r].push_back(cur);
+				}
+			}
+			fr(r,tam) fr(l,r+1) sort(all(mat[l][r]));
+			fr(i,sz(v)){
+				int l = 0;
+				while(l<tam){
+					int r = l;
+					while(r+1<tam and v[i][r+1]>=v[i][r]) r++;
+					ull cur = 0;
+					if(l) cur = hashes[i].gethash(0,l-1);
+					if(r<tam-1) cur += p27[l]*hashes[i].gethash(r+1,tam-1);
+					ans-=fcnt(cur,l,r)-1;
+					l = r+1;
+				}	
+			}
 		}
-		matid[ic].push_back(i+1);
-		matpref[ic].push_back(cnt);
 	}
-	
-	fr(qq,q){
-		ll id_f; string w; cin >> id_f >> w;
-		string s = s0;
-		ll id_i = 0;
-		while(sz(s)<sz(w) and id_i<id_f){
-			s = s + t[id_i] + s;
-			id_i++;
-		}
-		if(sz(s)<sz(w)){
-			cout << 0 << "\n";
-			continue;
-		}
-		meuhash hs(s), hw(w);
-		mb ans_cont = 0;
-		
-		fr(i,sz(s)-sz(w)+1){
-			if(hs.gethash(i,i+sz(w)-1)==hw.gethash(0,sz(w)-1)) ans_cont+=1;
-		}
-		ll dif = id_f-id_i;
-		ans_cont *= fp(mb(2),dif);
-		
-		mb ans_add = 0;		
-		vector<mb> addc(26);
-		fr(i,sz(w)){
-			if(i){
-				if(hw.gethash(0,i-1)!=hs.gethash(sz(s)-i,sz(s)-1)) continue;
-			}
-			if(i<sz(w)-1){
-				if(hw.gethash(i+1,sz(w)-1)!=hs.gethash(0,sz(w)-i-2)) continue;
-			}
-			int ic = w[i]-'a';
-			addc[ic] += 1;
-		}
-		fr(ic,26){
-			if(matid[ic].empty()) continue;
-			mb addcur = 0, subcur = 0;
-			int iif = upper_bound(all(matid[ic]),id_f) - matid[ic].begin();
-			if(iif==0) continue;
-			iif--;
-			ll diff = id_f-matid[ic][iif];
-			addcur = matpref[ic][iif]*fp(mb(2),diff);
-			
-			int iii = upper_bound(all(matid[ic]),id_i) - matid[ic].begin();
-			if(iii>0){ 
-				iii--;
-				ll difi = id_f-matid[ic][iii];
-				subcur = matpref[ic][iii]*fp(mb(2),difi);
-			}
-			
-			addcur -= subcur;
-			ans_add += addcur*addc[ic];
-		}
-		
-		mb ans = ans_cont + ans_add;
-		cout << ans.val << "\n";
-	}
+	cout << ans << "\n";
 }
