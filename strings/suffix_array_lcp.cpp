3b0709f73854db47a7ad47b764777d6ad3082499
VictorLamarca <victordslamarca@gmail.com>
2021-06-20 03:37:18 -0300

    Suffix array for vector int

--- strings/suffix_array_lcp.cpp
+++ strings/suffix_array_lcp.cpp
@@ -10,14 +10,23 @@ typedef long long ll;
 
 //solves https://codeforces.com/gym/103081/problem/K
 //also used in https://www.codechef.com/problems/ANUSAR
- 
+
+/*
+	Retorna vector p, suffix array
+	p[0] eh o indice do menor menor sufixo
+	para usar em vector de inteiros, fazer compressao de coordenadas
+	para [1,n] e o caracter especial adicionado sera o 0
+	Note que n eh incrementado e com caracter especial valores sao [0,n-1]
+*/
+//vector<int> make_suf(vector<int> s){
 vector<int> make_suf(string s){
 	s+=(char)0;
+	//s.push_back(0);
 	int n = sz(s);
 	vector<int> p(n), c(n), cnt(max(256,n));
 	
 	fr(i,n) cnt[s[i]]++;
-	for(int i = 1; i<256; i++) cnt[i]+=cnt[i-1];
+	for(int i = 1; i<max(n,256); i++) cnt[i]+=cnt[i-1];
 	fr(i,n) p[--cnt[s[i]]] = i;
 	int nc = 1;
 	for(int i = 1; i<n; i++){
@@ -46,7 +55,8 @@ vector<int> make_suf(string s){
 	
 	return p;
 }
- 
+
+//vector<int> make_lcp(vector<int> &s, vector<int> &p){
 vector<int> make_lcp(string &s, vector<int> &p){
 	int n = sz(s);
 	vector<int> rank(n), lcp(n-1);
